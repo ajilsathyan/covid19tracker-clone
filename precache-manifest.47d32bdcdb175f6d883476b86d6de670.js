23872ff56e03dfa7e5dec4226d23a37eb3243caf
Ajil Sathyan <ajilsathyan1998@gmail.com>
2020-10-24 11:09:30 +0530

    Updates

---
+++ precache-manifest.47d32bdcdb175f6d883476b86d6de670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c906b5cd2ed7e988c8ae4c3256b151",
+    "url": "/covid19tracker-clone/index.html"
+  },
+  {
+    "revision": "482a9e3ec5b18dccedb1",
+    "url": "/covid19tracker-clone/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "261e5df78bb089cf2437",
+    "url": "/covid19tracker-clone/static/css/main.bfc503ea.chunk.css"
+  },
+  {
+    "revision": "482a9e3ec5b18dccedb1",
+    "url": "/covid19tracker-clone/static/js/2.c707ed10.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/covid19tracker-clone/static/js/2.c707ed10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261e5df78bb089cf2437",
+    "url": "/covid19tracker-clone/static/js/main.a0d25a94.chunk.js"
+  },
+  {
+    "revision": "46ac89b80c9992f7b49b",
+    "url": "/covid19tracker-clone/static/js/runtime-main.259a5675.js"
+  }
+]);
